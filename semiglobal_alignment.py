af6c70745696fed7551f1573c28cf352a40449e5
Krzysztof Soja <krzysztof.soja@yahoo.com>
2019-02-03 20:06:30 +0100

    Nie przechodzi testów.

--- semiglobal_alignment.py
+++ semiglobal_alignment.py
@@ -2,96 +2,8 @@ import numpy as np
 from Bio import pairwise2
 from Bio import SeqIO
 import sys
-from tqdm import tqdm
-from numba import jit, njit
-
-
-class NeedlemanWunsch():
-
-
-
-    def __init__(self, sequnce1, sequnce2, penalty=-1):
-        pass
-
-    #https://numba.pydata.org/numba-doc/latest/reference/pysupported.html
-    #Koniecznie przeczytaj
-    @jit
-    def _set_start_variables(self):
-        self.matrix[0] = self.penalty * np.arange(0, self.matrix.shape[1])
-        self.matrix[:, 0] = self.penalty * np.arange(0, self.matrix.shape[0])
-
-
-    def _set_the_best_values(self, index_i, index_j):
-        pass
-
-
-    def _find_the_best_fit(self, values):
-        answer = [max(values)]
-        values.remove(answer[0])
-        for i in values[:-1]:
-            if i[0] == answer[0][0]:
-                answer.append(i)
-        return answer
-
-    @jit(nonpython=True)
-    def fill_matrix(self):
-        self._set_start_variables()
-        for i in tqdm(range(1, self.matrix.shape[0])):
-            for j in range(1, self.matrix.shape[1]):
-                self.h_values[0][i][j] = self.matrix[i - 1][j] + self.penalty
-                self.h_values[1][i][j] = self.matrix[i][j - 1] + self.penalty
-                if self.sequnce1[i - 1] == self.sequnce2[j - 1]:
-                    self.h_values[2][i][j] = self.matrix[i - 1][j - 1] + 1
-                else:
-                    self.h_values[2][i][j] = self.matrix[i - 1][j - 1] - 1
-                self.matrix[i][j] = max(self.h_values[:, i, j])
-
-    #Co tu się dzieje???
-    def _get_fit(self, index_i, index_j, number_of_fit, all_fit):
-        vectors = self.directions[index_i][index_j]
-        for i, vector in enumerate(vectors):
-            new_index_i = index_i + vector[0]
-            new_index_j = index_j + vector[1]
-            if i > 0:
-                all_fit.insert(number_of_fit + i, list(all_fit[number_of_fit]))
-            if new_index_i >= 1 and new_index_j >= 1:
-                all_fit[number_of_fit + i].append(self._get_char_to_fit((
-                    new_index_i, new_index_j), vector))
-                all_fit = self._get_fit(new_index_i, new_index_j,
-                                        number_of_fit + i, all_fit)
-        return all_fit
-
-    #???
-    def _get_char_to_fit(self, index, vector):
-        if vector == (-1, -1):
-            return (self.sequnce1[index[0] - 1], self.sequnce2[index[1] - 1])
-        elif vector == (-1, 0):
-            return (self.sequnce1[index[0] - 1], '-')
-        elif vector == (0, -1):
-            return ('-', self.sequnce2[index[1] - 1])
-
-    def get_fit(self):
-        all_fit = [[]]
-        index_i = len(self.directions) - 1
-        index_j = len(self.directions[1]) - 1
-        all_fit[0].append((self.sequnce1[index_i - 1],
-                           self.sequnce2[index_j - 1]))
-        all_fit = self._get_fit(index_i, index_j, 0, all_fit)
-
-        for fit in all_fit:
-            fit.reverse()
-
-        return all_fit
-
-    def print_as_string(tab_of_tuple):
-        str_seq1 = ''
-        str_seq2 = ''
-        for i in tab_of_tuple:
-            str_seq1 = str_seq1 + i[0]
-            str_seq2 = str_seq2 + i[1]
-        print(str_seq1)
-        print(str_seq2)
-
+from numba import njit
+from timer import Timer
 
 directions = {0: (-1, 0),
               1: (-1, -1),
@@ -110,19 +22,52 @@ def fill_matrix(matrix, h_values, seq1, seq2):
     for i in range(1, matrix.shape[0]):
         for j in range(1, matrix.shape[1]):
             h_values[0][i][j] = matrix[i - 1][j] - 1
-            h_values[1][i][j] = matrix[i][j - 1] - 1
+            h_values[2][i][j] = matrix[i][j - 1] - 1
             if seq1[i - 1] == seq2[j - 1]:
-                h_values[2][i][j] = matrix[i - 1][j - 1] + 1
+                h_values[1][i][j] = matrix[i - 1][j - 1] + 1
             else:
-                h_values[2][i][j] = matrix[i - 1][j - 1] - 1
+                h_values[1][i][j] = matrix[i - 1][j - 1] - 1
             matrix[i][j] = np.max(h_values[:, i, j])
 
 
-@njit
-def set_start_variables(matrix, gap_penalty):
+def set_start_matrix(matrix, gap_penalty):
     matrix[0] = gap_penalty * np.arange(0, matrix.shape[1])
     matrix[:, 0] = gap_penalty * np.arange(0, matrix.shape[0])
-    return matrix
+
+
+def set_start_h_values(h_values):
+    h_values[2][0] = np.ones((h_values.shape[2]))
+
+
+def get_one_of_the_best(matrix, h_values, seq1, seq2):
+    alignment1 = ""
+    alignment2 = ""
+    index_i, index_j = matrix.shape
+    index_i -= 1
+    index_j -= 1
+    index_seq1, index_seq2 = -1, -1
+    path = np.argmax(h_values, axis=0)
+    while not index_i == index_j == 0:
+        choice = path[index_i][index_j]
+        if choice == 1:
+            alignment1 = seq1[index_seq1] + alignment1
+            alignment2 = seq2[index_seq2] + alignment2
+            index_seq1 += directions[choice][0]
+            index_seq2 += directions[choice][1]
+            index_i += directions[choice][0]
+            index_j += directions[choice][1]
+        elif choice == 0:
+            alignment1 = seq1[index_seq1] + alignment1
+            alignment2 = '-' + alignment2
+            index_seq1 += directions[choice][0]
+            index_i += directions[choice][0]
+
+        else:
+            alignment1 = '-' + alignment1
+            alignment2 = seq2[index_seq2] + alignment2
+            index_j += directions[choice][1]
+            index_seq2 += directions[choice][1]
+    return alignment1, alignment2
 
 
 try:
@@ -134,16 +79,21 @@ except ValueError:
     sys.exit(0)
 
 
-seq_t = str(DNA_t.seq)
-seq_s = str(DNA_s.seq)
+seq_t = "CAGCACTTGGATTCTCGG" #str(DNA_t.seq)
+seq_s = "CAGCGTGG" #str(DNA_s.seq)
 gap_penalty = -1
 matrix = np.zeros((len(seq_t) + 1, len(seq_s) + 1), dtype=np.int32)
 h_values = np.zeros((3, len(seq_t) + 1, len(seq_s) + 1), dtype=np.int32)
 
+with Timer() as t:
+    set_start_matrix(matrix, gap_penalty)
+    set_start_h_values(h_values)
+    fill_matrix(matrix, h_values, seq_t, seq_s)
+    alignment = get_one_of_the_best(matrix, h_values, seq_t, seq_s)
 
-matrix = set_start_variables(matrix, gap_penalty)
-fill_matrix(matrix, h_values, seq_t, seq_s)
-print(matrix)
-print(h_values)
+
+print('Czas zadania wyniósł: %.03f sec.' % t.interval)
+print(alignment[0])
+print(alignment[1])
 
 #print(pairwise2.align.globalxx(human_str, rat_str))
