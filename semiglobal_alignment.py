e2af9cc4dd9685273049f1eb654381491f2b5e7c
Krzysztof Soja <krzysztof.soja@yahoo.com>
2019-02-04 17:05:45 +0100

    Kilka drobnych błędów do poprawy.

--- semiglobal_alignment.py
+++ semiglobal_alignment.py
@@ -28,21 +28,12 @@ def fill_matrix(matrix, h_values, seq1, seq2):
     for i in range(1, matrix.shape[0]):
         for j in range(1, matrix.shape[1]):
             h_values[0][i][j] = matrix[i - 1][j] - 1
-            h_values[2][i][j] = matrix[i][j - 1] - 1
+            h_values[1][i][j] = matrix[i][j - 1] - 1
             if seq1[i - 1] == seq2[j - 1]:
-                h_values[1][i][j] = matrix[i - 1][j - 1] + 1
+                h_values[2][i][j] = matrix[i - 1][j - 1] + 1
             else:
-                h_values[1][i][j] = matrix[i - 1][j - 1] - 1
+                h_values[2][i][j] = matrix[i - 1][j - 1] - 1
             matrix[i][j] = np.max(h_values[:, i, j])
-
-
-def set_start_matrix(matrix, gap_penalty):
-    matrix[0] = np.zeros(matrix.shape[1])
-    matrix[:, 0] = gap_penalty * np.arange(0, matrix.shape[0])
-
-
-def set_start_h_values(h_values):
-    h_values[2][0] = np.ones((h_values.shape[2]))
 
 
 def find_best_index(matrix):
@@ -56,54 +47,53 @@ def find_best_index(matrix):
 
 
 @njit
-def fill_begin_gap(matrix_shape, seq1, seq2, index_i, index_j):
-    alignment1 = ''
-    alignment2 = ''
-    if index_i < matrix_shape[0] - 1:
-        itr = 0
-        while itr < matrix_shape[0] - index_i:
-            alignment1 = seq1[itr] + alignment1
-            alignment2 = '-' + alignment2
-            itr += 1
-    elif index_j < matrix_shape[1] - 1:
-        itr = 0
-        while itr < matrix_shape[1] - index_j:
-            alignment2 = seq2[itr] + alignment2
-            alignment1 = '-' + alignment1
-            itr += 1
-    return alignment1, alignment2
-
-
-@njit
-def find_alignment(path, index_i, index_j, seq1, seq2):
+def find_alignment(path, best_i, best_j, seq1, seq2):
     alignment1 = ""
     alignment2 = ""
-    while not index_i == index_j == 0:
+
+    index_i, index_j = path.shape
+    index_i -= 1
+    index_j -= 1
+
+    while index_i > best_i:
+        alignment1 = seq1[index_i - 1] + alignment1
+        alignment2 = '-' + alignment2
+        index_i -= 1
+
+    while index_j > best_j:
+        alignment1 = '-' + alignment1
+        alignment2 = seq2[index_j - 1] + alignment2
+        index_j -= 1
+
+    while True:
         choice = path[index_i][index_j]
-        if choice == 1:
+        if choice == 2:
             alignment1 = seq1[index_i-1] + alignment1
             alignment2 = seq2[index_j-1] + alignment2
             index_i -= 1
+            index_j -= 1
+        elif choice == 1:
+            alignment1 = '-' + alignment1
+            alignment2 = seq2[index_j - 1] + alignment2
             index_j -= 1
         elif choice == 0:
-            alignment1 = seq1[index_i-1] + alignment1
+            alignment1 = seq1[index_i - 1] + alignment1
             alignment2 = '-' + alignment2
             index_i -= 1
-        else:
-            alignment1 = '-' + alignment1
-            alignment2 = seq2[index_j-1] + alignment2
-            index_j -= 1
+        if index_i == 0 and index_j == 0:
+            break
     return alignment1, alignment2
 
 
 def get_one_of_the_best(matrix, h_values, seq1, seq2):
     index_i, index_j = find_best_index(matrix)
     max_score = matrix[index_i][index_j]
+
     path = np.argmax(h_values, axis=0)
-    alignment1, alignment2 = fill_begin_gap(matrix.shape, seq1, seq2, index_i, index_j)
-    temp = find_alignment(path, index_i, index_j, seq1, seq2)
-    alignment1 += temp[0]
-    alignment2 += temp[1]
+    path[0] = np.ones_like(path[0])
+    path[:, 0] = np.zeros_like(path[:, 0])
+
+    alignment1, alignment2 = find_alignment(path, index_i, index_j, seq1, seq2)
     return max_score, alignment1, alignment2
 
 
@@ -117,19 +107,18 @@ except ValueError:
 
 seq_t = str(DNA_t.seq)
 seq_s = str(DNA_s.seq)
-seq_s, seq_t = min(seq_t, seq_s), max(seq_t, seq_s)
 gap_penalty = -1
 matrix = np.zeros((len(seq_s) + 1, len(seq_t) + 1), dtype=np.int32)
 h_values = np.zeros((3, len(seq_s) + 1, len(seq_t) + 1), dtype=np.int32)
 
 with Timer() as t:
-    set_start_matrix(matrix, gap_penalty)
-    set_start_h_values(h_values)
     fill_matrix(matrix, h_values, seq_s, seq_t)
     alignment = get_one_of_the_best(matrix, h_values, seq_s, seq_t)
 
 assert len(alignment[1]) == len(alignment[2])
 print('Czas rozwiązywania zadania wyniósł: %.03f sec.' % t.interval)
 
-text_to_file = str(alignment[0]) + '\n' + alignment[1] + "\n" + alignment[2]
+#text_to_file = str(alignment[0]) + '\n' + alignment[1] + "\n" + alignment[2]
+np.set_printoptions(threshold=np.inf)
+text_to_file = matrix
 save_to_file(text_to_file)
