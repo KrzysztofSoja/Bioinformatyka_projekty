3ce2bd5a550afe8ad797f1b4e252017229d84e6e
Krzysztof Soja <krzysztof.soja@yahoo.com>
2019-02-05 20:01:19 +0100

    MTGoaOA - Powinno działać.

--- maximizing_the_gap_symbols_of_an_optimal_alignment.py
+++ maximizing_the_gap_symbols_of_an_optimal_alignment.py
@@ -13,27 +13,23 @@ def read_data(path="/home/krzysztof/Pobrane/rosalind_mgap.txt"):
 
 @njit
 def fill_matrix(matrix, seq1, seq2):
-    h_values = np.zeros((3, matrix.shape[0], matrix.shape[1]))
+    h_values = np.zeros((3,))
     for i in range(1, matrix.shape[0]):
         for j in range(1, matrix.shape[1]):
-            h_values[0][i][j] = matrix[i - 1][j] - 1
-            h_values[1][i][j] = matrix[i][j - 1] - 1
-            if seq1[i - 1] == seq2[j - 1]:
-                h_values[2][i][j] = matrix[i - 1][j - 1] + 1
-            else:
-                h_values[2][i][j] = matrix[i - 1][j - 1] - 1
-            matrix[i][j] = np.max(h_values[:, i, j])
+            h_values[0] = matrix[i - 1][j] - 1
+            h_values[1] = matrix[i][j - 1] - 1
+            h_values[2] = matrix[i - 1][j - 1] + 1
+            matrix[i][j] = np.max(h_values)
 
 
 def calculate_max_gap(matrix):
-    return int(sum(matrix.shape) - 2*matrix[-1][-1])
+    return int(sum(matrix.shape)-2 - 2*matrix[-1][-1])
 
 
 with Timer() as t:
     seq1, seq2 = read_data()
     seq1, seq2 = str(seq1.seq), str(seq2.seq)
-    seq1, seq2 = "AACGTA", "ACACCTA"
-    alignment_matrix = np.zeros((len(seq1), len(seq2)))
+    alignment_matrix = np.zeros((len(seq1) + 1, len(seq2) + 1))
     alignment_matrix[0] = -np.arange(0, alignment_matrix.shape[1])
     alignment_matrix[:, 0] = -np.arange(0, alignment_matrix.shape[0])
     fill_matrix(alignment_matrix, seq1, seq2)
