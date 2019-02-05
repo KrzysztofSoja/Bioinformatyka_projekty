1ce996b10b220fb2296c0abc1f6826e0aa73e1db
Krzysztof Soja <krzysztof.soja@yahoo.com>
2019-02-05 06:05:27 +0100

    GAWPCaR - drobna refaktoryzacja.

--- genome_assembly_with_perfect_coverage_and_repeats.py
+++ genome_assembly_with_perfect_coverage_and_repeats.py
@@ -1,5 +1,4 @@
 import networkx as nx
-from functools import reduce
 
 
 def read_data(path="/home/krzysztof/Pobrane/rosalind_grep.txt"):
@@ -9,21 +8,16 @@ def read_data(path="/home/krzysztof/Pobrane/rosalind_grep.txt"):
 
 
 def build_graph(genomes):
-    graph = nx.DiGraph()
-    for genome_from in genomes:
-        for genome_to in genomes:
-            if genome_from != genome_to:
-                if all([True if cgf == cgt else False for cgf, cgt in zip(genome_from[1:], genome_to[:-1])]):
-                    graph.add_edge(genome_from[1:], genome_to[1:])
+    graph = nx.MultiDiGraph()
+    for genome in genomes[1:]:
+        graph.add_edge(genome[:-1], genome[1:])
     return graph
 
 
 def build_circular_strings(graph, string, len, output=[]):
     node = string[1-len:]
-    if node not in graph.nodes:
-        return output
     for neighbor in graph[node]:
-        copy_graph = nx.DiGraph(graph)
+        copy_graph = nx.MultiDiGraph(graph)
         copy_graph.remove_edge(node, neighbor)
         build_circular_strings(copy_graph, string + neighbor[-1], len, output)
         output.append(string)
@@ -32,11 +26,9 @@ def build_circular_strings(graph, string, len, output=[]):
 
 data = read_data()
 data = data.split('\n')
-del data[-1]
-print(len(data))
 
 bruijn_graph = build_graph(data)
-l = build_circular_strings(bruijn_graph, data[0], len(data[0]))
-print(l)
-l = [e for e in l if len(e) == len(data)-2]
-print(l)
+cycles = build_circular_strings(bruijn_graph, data[0], len(data[0]))
+cycles = [cycle for cycle in cycles if len(cycle) == len(data)]
+for cycle in cycles:
+    print(cycle[:-1])
